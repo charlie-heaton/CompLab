c50a4ab257d2139df3999d6cac48009d3afa3d5f
db1266 <72613103+db1266@users.noreply.github.com>
2020-10-28 23:38:39 +0000

    Update executable.sh
    COrrected output file names to be compatible with MgtoCa.sh

--- GULP/GULPfinal/executable.sh
+++ GULP/GULPfinal/executable.sh
@@ -2,8 +2,8 @@
 echo -e "Please enter the filepath of your gulp file: (e.g xx/yy/zz/gulp-5.2/Src/gulp)"
 read filepath
 ./MgtoCa.sh
-$filepath ./MgO
-$filepath ./MgO_Ca
+$filepath ./MgO_0Ca
+$filepath ./MgO_1Ca
 $filepath ./MgO_2Ca
 $filepath ./MgO_3Ca
 $filepath ./MgO_4Ca
