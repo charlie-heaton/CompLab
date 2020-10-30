c4fedaa2cc32e8a73b0d4e24798a4e6be56529fd
JC Cardwell <jcc544@york.ac.uk>
2020-10-30 12:24:28 +0000

    Added comments to code

--- NEW_DDPT/sourcecode/genlattice.f90
+++ NEW_DDPT/sourcecode/genlattice.f90
@@ -12,10 +12,10 @@ program GenLattice
     real(kind=dp), dimension(3) :: current_coordinates                                                    ! (Atom types: 1 = Mg, 2 = Ca, 3 = O)
 
     !Input variables through flags
-    IF (getoption('-type1',.true.,dummy)) THEN
-        read(dummy,*) atom1
-    ELSE
-        atom1 = "Mg"
+    IF (getoption('-type1',.true.,dummy)) THEN     !These if constructs check for flag inputs, and store 
+        read(dummy,*) atom1                        !them in variables. The default use of this program
+    ELSE                                           !is for Mg and Ca, but inputting other atom names will
+        atom1 = "Mg"                               !also work.
     END IF
     
     IF (getoption('-type2',.true.,dummy)) THEN
@@ -36,7 +36,7 @@ program GenLattice
         a2 = 4.76_dp
     END IF
     
-    
+    !The following section of code builds the Mg (or atom1) section of crystal
 
     i = 1
     current_coordinates = 0.0_dp
@@ -107,9 +107,8 @@ program GenLattice
       ycounter = 0
     end do
     
-  IF (getoption('-swaps',.true.,dummy)) THEN
-    read(dummy,*) swapfile
-    print *, swapfile
+  IF (getoption('-swaps',.true.,dummy)) THEN    !If we have inputted a swap file, then the swap function
+    read(dummy,*) swapfile                      !is now run.
     call swaps(swapfile)
   END IF
   
@@ -119,7 +118,8 @@ program GenLattice
     
     
   
-    
+  !The following code writes out the lattice to a DDPT compatible input file.
+  !The if statement is used to convert integer atom type into the atom abbreviation
         
   out_unit = 30
   open(file="input.pdb", unit = out_unit, status = "replace", iostat = istat, iomsg = errormsg)
@@ -162,20 +162,17 @@ program GenLattice
     out_unit = 40
     open(file=swapfile, unit=out_unit, status = "old", iostat = istat, iomsg = errormsg)
     do i = 1, 10000000
-      read(out_unit,*, end=800) index1, index2
-      if (istat < 0) then
-        exit
-      end if
+      read(out_unit,*, end=800) index1, index2 !The end label specifies where to jump to when file ends
       if ((coordinates(index1,5) == 3) .or. (coordinates(index2,5) == 3)) then
-        cycle
+        cycle      !We want to avoid swapping any oxygen atoms around
       end if
-      temp(1) = coordinates(index1,2)
+      temp(1) = coordinates(index1,2)  !Store the first atoms coordinates
       temp(2) = coordinates(index1,3)
       temp(3) = coordinates(index1,4)
-      coordinates(index1,2) = coordinates(index2,2)
+      coordinates(index1,2) = coordinates(index2,2) !Change the first atoms coordinates to the second
       coordinates(index1,3) = coordinates(index2,3)
       coordinates(index1,4) = coordinates(index2,4)
-      coordinates(index2,2) = temp(1)
+      coordinates(index2,2) = temp(1) !Move the second atom to the first set of coordinates
       coordinates(index2,3) = temp(2)
       coordinates(index2,4) = temp(3)
     end do  
