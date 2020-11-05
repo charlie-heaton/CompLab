051595f92a3a81049666c4a3973bf19dc9988c39
charlie-heaton <cah577@york.ac.uk>
2020-11-05 14:48:08 +0000

    Updating Results and ddpt-parrallel shell script

--- Shell/ddpt-process-parallel.sh
+++ Shell/ddpt-process-parallel.sh
@@ -11,11 +11,10 @@ PLOT=$DDPT/plot
 echo Running DDPT for Ca Mg Oxide Mixing Project >> log
 echo -e '\t'`hostname` at `date`>> log
 
-echo Extracting $END lines
+echo Extracting $1 lines from the big swap file
 
 START=1
-
-awk -v start="$START" -v end="$END" 'NR>=start&&NR<=end' $HOME/$2 > swapfile
+awk -v start="$START" -v end="$1" 'NR>=start&&NR<=end' $HOME/$2 > swapfile
 
 #Generate the input file 
 \time --output=log --append -f "Program: %C\nTotal time: %E" $BIN/GENLATTICE -a1 4.380 -a2 4.760 -type1 Mg -type2 Ca -swaps swapfile
@@ -38,8 +37,8 @@ awk -v start="$START" -v end="$END" 'NR>=start&&NR<=end' $HOME/$2 > swapfile
 \time --output=log --append -f "Program: %C\nTotal time: %E" python $PLOT/plot_dist_cross.py
 
 #Calculate the mode energy for a mode summation of 30 modes.
-awk 'NR>1 {print $4}' mode.energy >> energy_30_$END
-echo "$END" >> energy_sum_$END
-echo $(sed 's/$/+/' energy_30_$END) 0 | bc >> energy_sum 
+awk 'NR>1 {print $4}' mode.energy >> energy_30_$1
+echo "$1" >> energy_sum
+echo $(sed 's/$/+/' energy_30_$1) 0 | bc >> energy_sum 
 echo The free energy of the configuration is $(sed 's/$/+/' energy_30) 0 | bc >> log
 rm matrix.* mode.* energy_30*
