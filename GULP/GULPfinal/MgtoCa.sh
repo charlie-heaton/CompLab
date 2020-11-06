1e4833d44370c87df409ce5cb8eda7cb8a2fa53d
cc1769 <72613153+cc1769@users.noreply.github.com>
2020-11-06 15:04:31 +0000

    Update MgtoCa.sh

--- GULP/GULPfinal/MgtoCa.sh
+++ GULP/GULPfinal/MgtoCa.sh
@@ -5,10 +5,9 @@
 for i in  0 1 2 3 4
 do
 #Inputs variable to MgOtoCa.f90, and puts output in new .dat file
-    rm -f MgOtoCa.in
+    rm -f MgtoCa.in
     echo $i> MgOtoCa.in
-    ./a.out < MgOtoCa.in 
+    ./MgOtoCa < MgOtoCa.in
     mv MgOtoCa.dat MgO_${i}Ca.gin
 
 done
-
